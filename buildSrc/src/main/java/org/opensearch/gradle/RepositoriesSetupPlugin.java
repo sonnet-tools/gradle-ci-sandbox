1ddf2c75a601874be52757058425dbc262be2f83
myogahunter <myogahunter@gmail.com>
2026-07-05 23:44:27 +0300

    Add snapshot repository for integration builds

--- buildSrc/src/main/java/org/opensearch/gradle/RepositoriesSetupPlugin.java
+++ buildSrc/src/main/java/org/opensearch/gradle/RepositoriesSetupPlugin.java
@@ -84,6 +84,13 @@ public class RepositoriesSetupPlugin implements Plugin<Project> {
         }
         repos.mavenCentral();
 
+        // OpenSearch project snapshots
+        repos.maven(repo -> {
+            repo.setName("opensearch-snapshots");
+            repo.setUrl("https://ci.opensearch.org/ci/dbc/snapshots/opensearch/");
+        });
+
+
         String luceneVersion = VersionProperties.getLucene();
         if (luceneVersion.contains("-snapshot")) {
             // extract the revision number from the version with a regex matcher
